bef44c8cb7c7d965f6cb93d7f42ee2e8611fc462
VOGUE-original <solikamsk20001@gmail.com>
2022-03-27 23:33:12 +0500

    Third commit

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import sys
 from ctypes import *
 
 
-lib = cdll.LoadLibrary(r"Dll\x64\DllPastFuction.dll")
+bit_depth_of_Python = "x64" if sys.maxsize > 2 ** 32 else "x86"
+
+lib = cdll.LoadLibrary(fr"Dll\{bit_depth_of_Python}\DllPastFuction.dll")
 lib.FunctionPrint1()
 print()
 lib.FunctionPrint2()
 print()
 lib.FunctionPrint3()
 
-print(ffsdf)
 
+"Функции"
+lib.sum_int(a, b)  # Складывает два int
+lib.sum_double(a, b)  # Складывает два double
+lib.sum_char(a, b)  # Складывает два char
+lib.is_true(t)  # Проверяет, является ли t = True1
